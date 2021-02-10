be505c22cee39bb65e312b3d43ee848cf2324e47
Mitesh, Dhiran <mitesh_dhiran@hotmail.com>
2021-02-10 15:55:21 +0530

    Added additional test case. Created a base class so that process decorator can be centralized

--- Test/RequestDecoratorExtensionTest.cs
+++ Test/RequestDecoratorExtensionTest.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Threading.Tasks;
 using CommandDecoratorExtension;
 using FluentValidation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using RequestDecorator.Functional;
 
 namespace RequestDecorator.Extension.Test
@@ -12,9 +16,10 @@ namespace RequestDecorator.Extension.Test
     [TestClass]
     public class RequestDecoratorExtensionTest
     {
+
         public APIContext<Context> GetAPIContext()
         {
-            var apiContext = new APIContext<Context>(new Context(), SerializeDeserializeHelper.GetJSONSerializedObject
+            var apiContext = new APIContext<Context>(new Context(), SerializationHelper.CustomGetJSONSerializedObject
                 , (l) =>
                 {
                     Console.WriteLine(l.ToString());
@@ -47,6 +52,23 @@ namespace RequestDecorator.Extension.Test
             catch (System.AggregateException agg)
             {
                 Assert.IsTrue(agg.InnerException  is  FluentValidation.ValidationException);
+            }
+        }
+
+        [TestMethod]
+        public void QueryRequestDerivedFromBase_ValidationExceptionIsThrownWhenDataIsInvalid()
+        {
+            var apiContext = GetAPIContext();
+            var req = new QueryRequestDerivedFromBase(new QueryData(null));
+            try
+            {
+                var res = req.Process(apiContext).Result;
+                Assert.Fail(
+                    "Validation exception should be raised as null value is assigned to ID property of Query Data");
+            }
+            catch (System.AggregateException agg)
+            {
+                Assert.IsTrue(agg.InnerException is FluentValidation.ValidationException);
             }
         }
     }
@@ -87,14 +109,11 @@ namespace RequestDecorator.Extension.Test
         {
             Data = data;
         }
-        public Func<IRequestContext<QueryData, Model, Context>, MayBe<ValidationException>> ValidationFunc => (reqContext) =>
-        {
-            return reqContext.RequestInfo.Data.ID.HasValue
-                ? new MayBe<FluentValidation.ValidationException>(MayBeDataState.DataNotPresent)
-                :
-                new MayBe<FluentValidation.ValidationException>(new FluentValidation.ValidationException("ID Value is null"));
-
-        };
+        public Func<IRequestContext<QueryData, Model, Context>, MayBe<ValidationException>> ValidationFunc => (reqContext) 
+            => reqContext.RequestInfo.Data.ID.HasValue
+            ? new MayBe<FluentValidation.ValidationException>(MayBeDataState.DataNotPresent)
+            :
+            new MayBe<FluentValidation.ValidationException>(new FluentValidation.ValidationException("ID Value is null"));
         public QueryData Data { get; }
 
         public Func<IRequestContext<QueryData, Model, Context>, Task<Result<Model>>> ProcessRequestFunc =>
@@ -105,14 +124,83 @@ namespace RequestDecorator.Extension.Test
             }; 
         public async Task<Model> Process(IAPIContext<Context> context)
         {
-            var decoratedFunc = this.ProcessRequestFunc.DecorateRequestWithFluentValidation(this.ValidationFunc).DecorateRequestWithInputOutputLogging(SerializeDeserializeHelper.GetJSONSerializedObject).DecorateWithExecutionTimeLogger();
+            var decoratedFunc = this.ProcessRequestFunc.DecorateRequestWithFluentValidation(this.ValidationFunc).DecorateRequestWithInputOutputLogging(SerializationHelper.CustomGetJSONSerializedObject).DecorateWithExecutionTimeLogger();
             var res = await decoratedFunc(new RequestWithContext<QueryData, Model, Context>(context, this));
             var retVal = res.GetValueThrowExceptionIfExceptionPresent();
             return retVal;
         }
+    }
 
-        
+    /// <summary>
+    /// Base class which contains default process function which decorates the main function with various decorators
+    /// </summary>
+    /// <typeparam name="TI"></typeparam>
+    /// <typeparam name="TR"></typeparam>
+    /// <typeparam name="TC"></typeparam>
 
-        
+    public abstract class RequestWithValidationBase<TI,TR,TC> : IRequestWithFluentValidator<TI, TR, TC>
+    {
+        protected RequestWithValidationBase(TI data)
+        {
+            Data = data;
+        }
+        [JsonIgnore]
+        public abstract Func<IRequestContext<TI, TR, TC>, MayBe<ValidationException>> ValidationFunc { get; }
+        public TI Data { get; }
+        [JsonIgnore]
+        public abstract Func<IRequestContext<TI, TR, TC>, Task<Result<TR>>> ProcessRequestFunc { get; }
+
+        public async Task<TR> Process(IAPIContext<TC> context)
+        {
+            var decoratedFunc = this.ProcessRequestFunc.DecorateRequestWithFluentValidation(this.ValidationFunc).DecorateRequestWithInputOutputLogging(SerializationHelper.CustomGetJSONSerializedObject).DecorateWithExecutionTimeLogger();
+            var res = await decoratedFunc(new RequestWithContext<TI, TR, TC>(context, this));
+            var retVal = res.GetValueThrowExceptionIfExceptionPresent();
+            return retVal;
+        }
+    }
+
+    public class QueryRequestDerivedFromBase : RequestWithValidationBase<QueryData,Model,Context>
+    {
+        public QueryRequestDerivedFromBase(QueryData data) : base(data)
+        {
+        }
+
+       // [JsonIgnore]
+        public override Func<IRequestContext<QueryData, Model, Context>, MayBe<ValidationException>> ValidationFunc =>
+            (IRequestContext<QueryData, Model, Context>  req) =>
+                req.RequestInfo.Data.ID.HasValue
+                    ? new MayBe<FluentValidation.ValidationException>(MayBeDataState.DataNotPresent)
+                    :
+                    new MayBe<FluentValidation.ValidationException>(new FluentValidation.ValidationException("ID Value is null"));
+
+        //[JsonIgnore]
+        public override Func<IRequestContext<QueryData, Model, Context>, Task<Result<Model>>> ProcessRequestFunc
+        =>
+            (r) =>
+            {
+                var model = new Model(r.RequestInfo.Data.ID ?? -1);
+                return Task.FromResult(new Result<Model>(model));
+            };
+    }
+
+    public static class SerializationHelper
+    {
+        public static string CustomGetJSONSerializedObject(object value)
+        {
+            try
+            {
+                if (value == null)
+                    return string.Empty;
+                var sb = new StringBuilder();
+                var serializer = new JsonSerializer();
+                serializer.Serialize(new StringWriter(sb), value);
+                return sb.ToString();
+            }
+            catch(Exception ex)
+            {
+                return $"Could not serialize. Error: {ex.Message}";
+            }
+            return "Could not serialize";
+        }
     }
 }
